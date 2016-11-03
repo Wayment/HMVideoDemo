d59aa201d5ac734f6a74cf640cdf4351fd76b8cd
lianghuiming <lianghuiming@appgame.com>
2016-11-03 18:16:29 +0800

    视频流

--- HMVideoDemo/HMVideoDemo/ViewController.m
+++ HMVideoDemo/HMVideoDemo/ViewController.m
@@ -7,8 +7,23 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+#define HMScreenWidth   [UIScreen mainScreen].bounds.size.width
+#define HMScreenHeight  [UIScreen mainScreen].bounds.size.height
+
+@interface ViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
+
+/// 摄像头input
+@property (nonatomic, strong) AVCaptureDeviceInput *deviceInput;
+/// 捕获的视频流
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+/// 预览的图层
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
+/// 图片探测器
+@property (nonatomic, strong) CIDetector *detector;
+
+@property (nonatomic, strong) CIContext *ciContext;
 
 @end
 
@@ -16,14 +31,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.ciContext = [[CIContext alloc] initWithOptions:nil];
+    
+    // 获取摄像头设备  iSO:AVCaptureDeviceDiscoverySession
+    AVCaptureDevice *capDevice = nil;
+    NSArray *capDevices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for (AVCaptureDevice *device in capDevices) {
+        if (device.position == AVCaptureDevicePositionFront) {
+            capDevice = device;
+            break;
+        }
+    }
+    // 获取摄像头input
+    self.deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:capDevice error:nil];
+    if (!self.deviceInput) {
+        return;
+    }
+    
+    // 设置捕获视频流的大小
+    self.captureSession = [[AVCaptureSession alloc] init];
+    [self.captureSession setSessionPreset:AVCaptureSessionPresetiFrame960x540];
+    [self.captureSession addInput:self.deviceInput];
+    
+    // 预览
+    self.previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.captureSession];
+    self.previewLayer.frame = CGRectMake(0, 0, HMScreenWidth, HMScreenWidth);
+    self.previewLayer.backgroundColor = [UIColor lightGrayColor].CGColor;
+    [self.view.layer addSublayer:self.previewLayer];
+    
+    // 监听流
+    AVCaptureVideoDataOutput *videoOutput = [[AVCaptureVideoDataOutput alloc] init];
+    [self.captureSession addOutput:videoOutput];
+    [videoOutput setSampleBufferDelegate:self queue:dispatch_get_main_queue()];
+    
+
+    // 开启流
+    [self.captureSession startRunning];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - <AVCaptureVideoDataOutputSampleBufferDelegate>
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+    
+    CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    
+    UIImage *image = [UIImage imageNamed:@"common_icon_living"];
+    CIImage *cImage = [CIImage imageWithCGImage:image.CGImage];
+    [self.ciContext render:cImage toCVPixelBuffer:pixelBuffer];
 }
-
 
 @end
