5cd21078f979e19952d79b4285a899dc96a62251
lianghuiming <lianghuiming@appgame.com>
2016-11-03 14:30:11 +0800

    项目初始化

---
+++ HMVideoDemo/HMVideoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMVideoDemo
+//
+//  Created by Huiming on 16/11/3.
+//  Copyright © 2016年 AppGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HMVideoDemo/HMVideoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMVideoDemo
+//
+//  Created by Huiming on 16/11/3.
+//  Copyright © 2016年 AppGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
